7c46843ba813fa7dddb234df04b9d94e39978a69
unknown <kraviraj407@gmail.com>
2025-06-24 23:40:23 +0530

    Day 1

---
+++ day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+C:\Users\aa\Documents\Rust\rust_notebook\day1\target\debug\day1.exe: C:\Users\aa\Documents\Rust\rust_notebook\day1\src\main.rs
---
+++ day1/target/debug/deps/day1-bea7897c7262de99.d
@@ -0,0 +1,5 @@
+C:\Users\aa\Documents\Rust\rust_notebook\day1\target\debug\deps\libday1-bea7897c7262de99.rmeta: src\main.rs
+
+C:\Users\aa\Documents\Rust\rust_notebook\day1\target\debug\deps\day1-bea7897c7262de99.d: src\main.rs
+
+src\main.rs:
